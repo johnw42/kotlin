b35f8868ecdfc1dc67c83a9e0d03cbb7d91a4d0c
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-04-12 16:24:05 +0300

    Exclude interface methods with body from 'isAbstractMethod' fun for java 8 target

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/InlineCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/InlineCodegen.java
@@ -307,12 +307,13 @@ public class InlineCodegen extends CallGenerator {
         }
         KtNamedFunction inliningFunction = (KtNamedFunction) element;
 
-        MethodNode node = new MethodNode(InlineCodegenUtil.API,
-                                       getMethodAsmFlags(functionDescriptor, context.getContextKind()) | (callDefault ? Opcodes.ACC_STATIC : 0),
-                                       asmMethod.getName(),
-                                       asmMethod.getDescriptor(),
-                                       null,
-                                       null);
+        MethodNode node = new MethodNode(
+                InlineCodegenUtil.API,
+                getMethodAsmFlags(functionDescriptor, context.getContextKind(), state) | (callDefault ? Opcodes.ACC_STATIC : 0),
+                asmMethod.getName(),
+                asmMethod.getDescriptor(),
+                null,
+                null);
 
         //for maxLocals calculation
         MethodVisitor maxCalcAdapter = InlineCodegenUtil.wrapWithMaxLocalCalc(node);
@@ -451,7 +452,7 @@ public class InlineCodegen extends CallGenerator {
 
         JvmMethodSignature jvmMethodSignature = typeMapper.mapSignatureSkipGeneric(descriptor);
         Method asmMethod = jvmMethodSignature.getAsmMethod();
-        MethodNode methodNode = new MethodNode(InlineCodegenUtil.API, getMethodAsmFlags(descriptor, context.getContextKind()), asmMethod.getName(), asmMethod.getDescriptor(), null, null);
+        MethodNode methodNode = new MethodNode(InlineCodegenUtil.API, getMethodAsmFlags(descriptor, context.getContextKind(), state), asmMethod.getName(), asmMethod.getDescriptor(), null, null);
 
         MethodVisitor adapter = InlineCodegenUtil.wrapWithMaxLocalCalc(methodNode);
 
@@ -648,7 +649,7 @@ public class InlineCodegen extends CallGenerator {
 
     @Override
     public void putHiddenParams() {
-        if ((getMethodAsmFlags(functionDescriptor, context.getContextKind()) & Opcodes.ACC_STATIC) == 0) {
+        if ((getMethodAsmFlags(functionDescriptor, context.getContextKind(), state) & Opcodes.ACC_STATIC) == 0) {
             invocationParamBuilder.addNextParameter(AsmTypes.OBJECT_TYPE, false, null);
         }
 
