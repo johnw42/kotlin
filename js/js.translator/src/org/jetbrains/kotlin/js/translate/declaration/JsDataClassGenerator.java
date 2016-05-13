a613a0b7d43d64aac2cdde56930fa82d4f27527c
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-05-13 14:02:22 +0300

    KT-11960 Fix for data classes

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/declaration/JsDataClassGenerator.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/declaration/JsDataClassGenerator.java
@@ -33,16 +33,11 @@ import java.util.List;
 class JsDataClassGenerator extends DataClassMethodGenerator {
     private final TranslationContext context;
     private final List<? super JsPropertyInitializer> output;
-    private final List<JsName> closureFieldNames = new ArrayList<JsName>();
 
     JsDataClassGenerator(KtClassOrObject klass, TranslationContext context, List<? super JsPropertyInitializer> output) {
         super(klass, context.bindingContext());
         this.context = context;
         this.output = output;
-    }
-
-    public void addClosureVariable(@NotNull JsName name) {
-        closureFieldNames.add(name);
     }
 
     @Override
@@ -60,10 +55,6 @@ class JsDataClassGenerator extends DataClassMethodGenerator {
         assert function.getValueParameters().size() == constructorParameters.size();
 
         List<JsExpression> constructorArguments = new ArrayList<JsExpression>(constructorParameters.size());
-
-        for (JsName closureFieldName : closureFieldNames) {
-            constructorArguments.add(propertyAccessor(JsLiteral.THIS, closureFieldName.getIdent()));
-        }
 
         for (int i = 0; i < constructorParameters.size(); i++) {
             KtParameter constructorParam = constructorParameters.get(i);
@@ -93,6 +84,9 @@ class JsDataClassGenerator extends DataClassMethodGenerator {
         JsExpression constructorRef = context.getQualifiedReference(constructor);
 
         JsExpression returnExpression = new JsNew(constructorRef, constructorArguments);
+        if (context.shouldBeDeferred(constructor)) {
+            context.deferConstructorCall(constructor, constructorArguments);
+        }
         functionObj.getBody().getStatements().add(new JsReturn(returnExpression));
     }
 
