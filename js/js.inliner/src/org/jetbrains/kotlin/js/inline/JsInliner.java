92539f49ee7604d6b78cdefc53fd63a02351d340
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-05-31 11:56:53 +0300

    ~~~ fixup previous

--- js/js.inliner/src/org/jetbrains/kotlin/js/inline/JsInliner.java
+++ js/js.inliner/src/org/jetbrains/kotlin/js/inline/JsInliner.java
@@ -19,7 +19,6 @@ package org.jetbrains.kotlin.js.inline;
 import com.google.dart.compiler.backend.js.ast.*;
 import com.google.dart.compiler.backend.js.ast.metadata.MetadataProperties;
 import com.intellij.psi.PsiElement;
-import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -256,13 +255,7 @@ public class JsInliner extends JsVisitorWithContextImpl {
         @Override
         public NamingContext newNamingContext() {
             JsScope scope = getFunctionContext().getScope();
-            return new NamingContext(scope, new Function1<List<? extends JsStatement>, Unit>() {
-                @Override
-                public Unit invoke(List<? extends JsStatement> statements) {
-                    getStatementContext().addPrevious(statements);
-                    return null;
-                }
-            });
+            return new NamingContext(scope, getStatementContext());
         }
 
         @NotNull
